1d3a3ad512c66fc9b66091542c3859e7cb0fd683
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-17 21:11:26 +0800

    master

--- 162.Find Peak Element.cpp
+++ 162.Find Peak Element.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-03-17 19:09:56
- * @LastEditTime: 2021-03-17 20:44:57
+ * @LastEditTime: 2021-03-17 21:07:55
  * @FilePath: \Leetcode\162.Find Peak Element.cpp
  */
 /*
@@ -28,10 +28,26 @@ using namespace std;
 
 
 
+/*
+RESULT: Accept
+TIME:     0ms    BEAT: 100.00%    O(n) = 
+MEMORY: 8.4MB    BEAT:  98.58%    O(n) = 
+LAST EDIT TIME: 2021年3月17日21:3:32
+Description: 二分。
+*/
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-
+        if (nums.size() == 1) return 0;
+        int mid;
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            mid = (r - l) / 2 + l;
+            if (nums[mid] > nums[mid + 1]) r = mid;
+            else l = mid + 1;
+        }
+        return l;
     }
 };
 
@@ -67,18 +83,18 @@ LAST EDIT TIME: 2021年3月17日20:22:57
 Description: 
 */
 
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-        if (nums.size() == 1) return 0;
-        if (nums[0] > nums[1]) return 0;
-        for (int i = 1; i < nums.size() - 1; i ++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
-        }
-        if (nums.back() > nums[nums.size() - 2]) return nums.size() - 1;
-        return -1;
-    }
-};
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         if (nums.size() == 1) return 0;
+//         if (nums[0] > nums[1]) return 0;
+//         for (int i = 1; i < nums.size() - 1; i ++) {
+//             if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+//         }
+//         if (nums.back() > nums[nums.size() - 2]) return nums.size() - 1;
+//         return -1;
+//     }
+// };
 
 
 int main() {
