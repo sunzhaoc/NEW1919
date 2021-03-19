6fd2c18b20b1b252c8ed906f9cdff0b7d0a61d8a
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-19 15:51:39 +0800

    master

--- 166.Fraction to Recurring Decimal.cpp
+++ 166.Fraction to Recurring Decimal.cpp
@@ -1,4 +1,12 @@
 /*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-18 21:00:44
+ * @LastEditTime: 2021-03-19 15:51:24
+ * @FilePath: \Leetcode\166.Fraction to Recurring Decimal.cpp
+ */
+/*
  * @lc app=leetcode.cn id=166 lang=cpp
  *
  * [166] 分数到小数
@@ -11,5 +19,6 @@ public:
 
     }
 };
+
 // @lc code=end
 
--- demp.cpp
+++ demp.cpp
@@ -3,6 +3,13 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-03-16 21:00:58
- * @LastEditTime: 2021-03-16 21:00:58
+ * @LastEditTime: 2021-03-19 15:44:06
  * @FilePath: \Leetcode\demp.cpp
  */
+
+
+#include iostream
+using namespace std;
+{
+    
+} // namespace std;
