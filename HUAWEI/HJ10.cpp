479b225d8b3c22b381a26ad266b566bd58802759
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-23 20:33:53 +0800

    master

---
+++ HUAWEI/HJ10.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-23 20:33:38
+ * @LastEditTime: 2021-03-23 20:33:39
+ * @FilePath: \Leetcode\HUAWEI\HJ10.cpp
+ */
+#include <iostream>
+#include<unordered_set>
+using namespace std;
+
+int main() {
+    string st;
+    cin >> st;
+    unordered_set<char> charSet;
+    for (auto c: st) {
+        charSet.insert(c);
+    }
+    cout << charSet.size() << endl;
+    return 0;
+}
