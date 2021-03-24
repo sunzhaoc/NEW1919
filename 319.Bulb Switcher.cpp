04a320023f37b94203edacb289d2c170c090c6c3
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-24 18:14:02 +0800

    master

--- 319.Bulb Switcher.cpp
+++ 319.Bulb Switcher.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-03-24 09:02:41
- * @LastEditTime: 2021-03-24 18:07:34
+ * @LastEditTime: 2021-03-24 18:11:35
  * @FilePath: \Leetcode\319.Bulb Switcher.cpp
  */
 /*
@@ -28,18 +28,17 @@ using namespace std;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT: %    O(n) = 
-MEMORY: MB    BEAT: %    O(n) = 
+TIME:     0ms    BEAT: 100.00%    O(n) = 1
+MEMORY: 5.7MB    BEAT:  89.74%    O(n) = 1
 LAST EDIT TIME: 2021年3月24日17:56:37
-Description: 不会。Y总的代码。
-每个灯被按的次数等于其约数的个数 ---> 求1-n中约数个数为奇数的数的个数。
+Description: 不会。Y总的思路。
+每个灯被按的次数等于其约数的个数 ---> 求1-n中约数个数为奇数的数的个数 --> 只有平方数的约数个数为奇数
 */
 
 class Solution {
 public:
     int bulbSwitch(int n) {
-        if (n == 0) return 0;
-        
+        return sqrt(n);
     }
 };
 // @lc code=end
