8b9d76dccc611c3234933fbbdeb1f07ab64adb62
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-24 18:00:16 +0800

    amaster

--- 319.Bulb Switcher.cpp
+++ 319.Bulb Switcher.cpp
@@ -3,8 +3,8 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-03-24 09:02:41
- * @LastEditTime: 2021-03-24 09:04:23
- * @FilePath: \Leetcode\319.灯泡开关.cpp
+ * @LastEditTime: 2021-03-24 17:56:31
+ * @FilePath: \Leetcode\319.Bulb Switcher.cpp
  */
 /*
  * @lc app=leetcode.cn id=319 lang=cpp
@@ -30,15 +30,15 @@ using namespace std;
 RESULT: Accept
 TIME:   ms    BEAT: %    O(n) = 
 MEMORY: MB    BEAT: %    O(n) = 
-USED TIME: 
-LAST EDIT TIME: 
-Description: 
+LAST EDIT TIME: 2021年3月24日17:56:37
+Description: 不会。Y总的代码。
 */
 
 class Solution {
 public:
     int bulbSwitch(int n) {
-
+        if (n == 0) return 0;
+        
     }
 };
 // @lc code=end
