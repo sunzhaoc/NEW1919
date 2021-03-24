2785bf4a84cdea02775bd4c1c0dbea5b8a30b283
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-24 18:08:11 +0800

    master

--- 319.Bulb Switcher.cpp
+++ 319.Bulb Switcher.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-03-24 09:02:41
- * @LastEditTime: 2021-03-24 17:56:31
+ * @LastEditTime: 2021-03-24 18:07:34
  * @FilePath: \Leetcode\319.Bulb Switcher.cpp
  */
 /*
@@ -32,6 +32,7 @@ TIME:   ms    BEAT: %    O(n) =
 MEMORY: MB    BEAT: %    O(n) = 
 LAST EDIT TIME: 2021年3月24日17:56:37
 Description: 不会。Y总的代码。
+每个灯被按的次数等于其约数的个数 ---> 求1-n中约数个数为奇数的数的个数。
 */
 
 class Solution {
