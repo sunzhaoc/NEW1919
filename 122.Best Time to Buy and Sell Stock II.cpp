8714cf3773eb92f17937a4161dd56a532cc75cf8
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-25 17:14:12 +0800

    master

---
+++ 122.Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,69 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-25 16:57:20
+ * @LastEditTime: 2021-03-25 17:13:38
+ * @FilePath: \Leetcode\122.Best Time to Buy and Sell Stock II.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=122 lang=cpp
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:      8ms    BEAT: 54.55%    O(n) = 
+MEMORY: 12.6MB    BEAT: 57.29%    O(n) = 
+USED TIME: 14:05
+LAST EDIT TIME: 2021年3月25日17:13:17
+Description: 二刷。之前是python。
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 0;
+        int res = 0;
+        while (r < prices.size()) {
+            if (r + 1 < prices.size() && prices[r + 1] <= prices[r]) {
+                if (l != r) res += prices[r] - prices[l];
+                l = r + 1;
+            }
+            r ++;
+            
+        }
+        if (l != r - 1) res += prices[r - 1] - prices[l];
+        
+        return res;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1,2,3,4,5};
+    int ans = sol.maxProfit(nums);
+    cout << ans << endl; 
+    system("pause");
+    return 0;
+}
+
+// @lc code=end
+
