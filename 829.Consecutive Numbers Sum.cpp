36faa5f832b12511ca9aac49a74ddf6f0f1d9436
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-26 10:41:18 +0800

    maseter

--- 829.Consecutive Numbers Sum.cpp
+++ 829.Consecutive Numbers Sum.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-03-24 09:03:29
- * @LastEditTime: 2021-03-24 09:04:46
+ * @LastEditTime: 2021-03-26 10:36:19
  * @FilePath: \Leetcode\829.连续整数求和.cpp
  */
 /*
@@ -28,18 +28,22 @@ using namespace std;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT: %    O(n) = 
-MEMORY: MB    BEAT: %    O(n) = 
-USED TIME: 
-LAST EDIT TIME: 
-Description: 
+TIME:     8ms    BEAT: 17.96%    O(n) = n^(1/2)
+MEMORY: 5.8MB    BEAT: 63.11%    O(n) = 1
+LAST EDIT TIME: 2021年3月26日10:35:52
+Description: 不会。纯数学题。
 */
 
 class Solution {
 public:
     int consecutiveNumbersSum(int N) {
-
+        int res = 0;
+        for (int b = 1; b * (b - 1) < 2 * N; b ++) {
+            if (2 * N % b == 0 && (2 * N / b - b + 1) % 2 == 0) res ++;
+        }
+        return res;
     }
 };
+
 // @lc code=end
 
