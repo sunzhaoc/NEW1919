aef4b47b332888b3f453027d23cba1a4b5b2d04e
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-26 09:54:04 +0800

    master

--- 223.Rectangle Area.cpp
+++ 223.Rectangle Area.cpp
@@ -1,10 +1,19 @@
 /*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-24 09:03:19
+ * @LastEditTime: 2021-03-26 09:54:00
+ * @FilePath: \Leetcode\223.Rectangle Area.cpp
+ */
+/*
  * @lc app=leetcode.cn id=223 lang=cpp
  *
  * [223] 矩形面积
  */
 
 // @lc code=start
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -22,9 +31,8 @@ using namespace std;
 RESULT: Accept
 TIME:   ms    BEAT: %    O(n) = 
 MEMORY: MB    BEAT: %    O(n) = 
-USED TIME: 
-LAST EDIT TIME: 
-Description: 
+LAST EDIT TIME: 2021年3月26日9:53:47
+Description: 不会。。。就离谱。
 */
 
 class Solution {
