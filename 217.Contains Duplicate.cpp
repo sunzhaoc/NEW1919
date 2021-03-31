fd3cb282ae48943027ad96336d29365a13bf6839
sunzhaoc <sunzhaoc_2011@163.com>
2021-03-31 09:46:18 +0800

    master

---
+++ 217.Contains Duplicate.cpp
@@ -0,0 +1,54 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-03-31 09:40:46
+ * @LastEditTime: 2021-03-31 09:45:53
+ * @FilePath: \Leetcode\217.Contains Duplicate.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=217 lang=cpp
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:     32ms    BEAT: 77.78%    O(n) = 
+MEMORY: 19.5MB    BEAT: 56.94%    O(n) = 
+USED TIME: 04:12
+LAST EDIT TIME: 2021年3月31日9:45:35
+Description: 
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i ++) {
+            int a = s.size();
+            s.insert(nums[i]);
+            int b = s.size();
+            if (a == b) return true;
+        }
+        return false;
+    }
+};
+
+// @lc code=end
+
