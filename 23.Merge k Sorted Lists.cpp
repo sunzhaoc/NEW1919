68f9b8022b7819dae48598895371354d449b9eb0
sunzhaoc <sunzhaoc_2011@163.com>
2021-04-06 21:11:19 +0800

    master

--- 23.Merge k Sorted Lists.cpp
+++ 23.Merge k Sorted Lists.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: Vicro
  * @Date: 2021-04-06 20:42:41
- * @LastEditTime: 2021-04-06 20:45:17
+ * @LastEditTime: 2021-04-06 21:09:40
  * @FilePath: \Leetcode\23.Merge k Sorted Lists.cpp
  */
 /*
@@ -22,10 +22,55 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/*
+RESULT: Accept
+TIME:   20ms    BEAT: 97.28%    O(n) = 
+MEMORY: 13MB    BEAT: 55.38%    O(n) = 
+USED TIME: 21:17
+LAST EDIT TIME: 2021年4月6日21:9:17
+Description: Easy
+最小堆。
+*/
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        struct cmp {
+            bool operator() (const pair<int, int>& x, const pair<int, int>& y) {
+                return x.first > y.first;
+            }
+        };
 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> minHeap; // val, id
+
+        if (lists.empty()) return nullptr;
+        int n;
+        for (int i = 0; i < lists.size(); i ++) {
+            if (lists[i] != nullptr) n ++;
+        }
+        if (n == 0) return nullptr;
+        for (int i = 0; i < lists.size(); i ++) {
+            if (lists[i] == nullptr) continue;
+            minHeap.push(make_pair(lists[i]->val, i));
+        }
+
+        ListNode* node = new ListNode(-1);
+        ListNode* cur = node;
+        while (minHeap.size()) {
+            int id = minHeap.top().second;
+            // ListNode* tmp = lists[id]->next;
+            cur->next = lists[id];
+            cur = cur->next;
+            lists[id] = lists[id]->next;
+            minHeap.pop();
+            if (lists[id]) minHeap.push(make_pair(lists[id]->val, id));
+        }
+
+        return node->next;
     }
 };
 // @lc code=end
