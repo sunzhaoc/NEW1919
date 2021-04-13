dcd51f001472a7780fe152481c43dbb58dca0a3d
sunzhaoc <sunzhaoc_2011@163.com>
2021-04-13 14:17:02 +0800

    amster

---
+++ 108.Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,91 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-04-13 14:09:35
+ * @LastEditTime: 2021-04-13 14:16:32
+ * @FilePath: \Leetcode\108.Convert Sorted Array to Binary Search Tree.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# define POW2(X) (1 << (X))
+# define CKBIT(S,X) (((S) & POW2(X)) != 0)
+const double pi = acos(-1.0);
+const double eps = 1e-11;
+const int MOD = 1e9 + 7;
+template<class T> inline void ckmin(T &a,T b){ a = min(a, b); }
+template<class T> inline void ckmax(T &a,T b){ a = max(a, b); }
+template<class T> inline T sqr(T x){ return x * x; }
+# define SZ(A) ((int)A.size())
+# define LENGTH(A) ((int)A.length())
+# define MP(A, B) make_pair(A, B)
+# define PB(X) push_back(X)
+# define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+# define REP(i, a) for(int i = 0; i < (a); ++i)
+# define ALL(A) A.begin(), A.end()
+# define F first
+# define S second
+# define V vector
+template<class T> int CMP(T a[], const T b[], int n) { return memcmp(a, b, n * sizeof(T)); }
+template<class T> void COPY(T a[], const T b[], int n) { memcpy(a, b, n * sizeof(T)); }
+template<class T> void SET(T a[], int val, int n) { memset(a, val, n*sizeof(T)); }
+using uint = unsigned int;
+using LL = long long;
+using ULL = unsigned long long;
+using PII = pair<int, int>;
+using VPII = vector<PII>;
+using PLL = pair<LL, LL>;
+using VPLL = vector<PLL>;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VD = vector<double>;
+using VS = vector<string>;
+using VVS = vector<VS>;
+
+
+/*
+RESULT: Accept
+TIME:     12ms    BEAT: 94.24%    O(n) = 
+MEMORY: 20.9MB    BEAT: 53.90%    O(n) = 
+USED TIME: 很快。
+LAST EDIT TIME: 2021年4月13日14:16:12
+Description: 
+*/
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return backTrack(nums, 0, SZ(nums) - 1);
+    }
+
+    TreeNode* backTrack(VI& nums, int l, int r) {
+        if (l > r) return nullptr;
+        int mid = (r - l) / 2 + l;
+        TreeNode* node = new TreeNode(nums[mid]);
+        node->left = backTrack(nums, l, mid - 1);
+        node->right = backTrack(nums, mid + 1, r);
+        return node;
+    }
+};
+// @lc code=end
+
---
+++ 110.Balanced Binary Tree.cpp
@@ -0,0 +1,93 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: Vicro
+ * @Date: 2021-04-13 14:00:36
+ * @LastEditTime: 2021-04-13 14:09:11
+ * @FilePath: \Leetcode\110.Balanced Binary Tree.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# define POW2(X) (1 << (X))
+# define CKBIT(S,X) (((S) & POW2(X)) != 0)
+const double pi = acos(-1.0);
+const double eps = 1e-11;
+const int MOD = 1e9 + 7;
+template<class T> inline void ckmin(T &a,T b){ a = min(a, b); }
+template<class T> inline void ckmax(T &a,T b){ a = max(a, b); }
+template<class T> inline T sqr(T x){ return x * x; }
+# define SZ(A) ((int)A.size())
+# define LENGTH(A) ((int)A.length())
+# define MP(A, B) make_pair(A, B)
+# define PB(X) push_back(X)
+# define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+# define REP(i, a) for(int i = 0; i < (a); ++i)
+# define ALL(A) A.begin(), A.end()
+# define F first
+# define S second
+# define V vector
+template<class T> int CMP(T a[], const T b[], int n) { return memcmp(a, b, n * sizeof(T)); }
+template<class T> void COPY(T a[], const T b[], int n) { memcpy(a, b, n * sizeof(T)); }
+template<class T> void SET(T a[], int val, int n) { memset(a, val, n*sizeof(T)); }
+using uint = unsigned int;
+using LL = long long;
+using ULL = unsigned long long;
+using PII = pair<int, int>;
+using VPII = vector<PII>;
+using PLL = pair<LL, LL>;
+using VPLL = vector<PLL>;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VD = vector<double>;
+using VS = vector<string>;
+using VVS = vector<VS>;
+
+
+/*
+RESULT: Accept
+TIME:      8ms    BEAT: 97.07%    O(n) = 
+MEMORY: 20.3MB    BEAT: 78.93%    O(n) = 
+USED TIME: 07:10
+LAST EDIT TIME: 2021年4月13日14:8:55
+Description: 
+*/
+
+class Solution {
+public:
+    bool ans = true;
+    bool isBalanced(TreeNode* root) {
+        int tmp = dfs(root, 0);
+        return ans;
+    }
+    
+    int dfs(TreeNode* node, int depth) {
+        if (node == nullptr) return depth - 1;
+        int left = dfs(node->left, depth + 1);
+        int right = dfs(node->right, depth + 1);
+        if (abs(right - left) > 1) ans = false;
+        return max(left, right);
+    }
+};
+
+// @lc code=end
+
