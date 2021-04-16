246fe1bd184b3b11a2e9ed25828cd592f7e2033c
sunzhaoc <sunzhaoc_2011@163.com>
2021-04-16 10:46:06 +0800

    master

--- changename.py
+++ changename.py
@@ -3,6 +3,23 @@ Description:
 Version: 1.0
 Autor: Vicro
 Date: 2021-04-16 10:33:35
-LastEditTime: 2021-04-16 10:33:35
+LastEditTime: 2021-04-16 10:45:15
 FilePath: \Leetcode\changename.py
 '''
+
+
+import os
+path = 'Y:\\2020\\Leetcode'
+
+file_name = os.listdir(path)
+
+for name in file_name:
+    if name[-2:] == "py" :
+        for s in name:
+            if s == '.':
+                index_num = name.index(s)
+                new_name = name[:index_num + 1] + name[index_num + 2:]
+                os.rename(os.path.join(path, name), os.path.join(path, new_name))
+                break
+
+a = 1
