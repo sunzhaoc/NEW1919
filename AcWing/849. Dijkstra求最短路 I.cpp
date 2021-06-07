49398bef0054ad552769dfb708b7bf062d06d8d5
sunzhaoc <sunzhaoc_2011@163.com>
2021-06-07 14:14:17 +0800

    master

---
+++ AcWing/849. Dijkstra求最短路 I.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: 冰凝水
+ * @Date: 2021-06-07 14:10:15
+ * @LastEditTime: 2021-06-07 14:14:14
+ * @FilePath: \Leetcode\AcWing\849. Dijkstra求最短路 I.cpp
+ */
+
+# include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    return 0;
+}
