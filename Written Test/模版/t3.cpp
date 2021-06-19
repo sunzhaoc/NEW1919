d3bf19453e03e608014409d22848506168d81a45
sunzhaoc <sunzhaoc_2011@163.com>
2021-06-19 09:56:11 +0800

    master

---
+++ Written Test/模版/t3.cpp
@@ -0,0 +1,97 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: 冰凝水
+ * @Date: 2021-04-25 10:19:28
+ * @LastEditTime: 2021-06-12 21:39:56
+ * @FilePath: \Leetcode\Week Competition\模版\t1.cpp
+ */
+
+/*
+ * 
+ * 　　┏┓　　　┏┓+ +
+ * 　┏┛┻━━━┛┻┓ + +
+ * 　┃　　　　　　　┃ 　
+ * 　┃　　　━　　　┃ ++ + + +
+ *  ██姜██━██狗██ ┃+
+ * 　┃　　　　　　　┃ +
+ * 　┃　　　┻　　　┃
+ * 　┃　　　　　　　┃ + +
+ * 　┗━┓　　　┏━┛
+ * 　　　┃　　　┃　　　　　　　　　　　
+ * 　　　┃　　　┃ + + + +
+ * 　　　┃　　　┃
+ * 　　　┃　　　┃ +  神兽保佑
+ * 　　　┃　　　┃    代码无bug　　
+ * 　　　┃　　　┃　　+　　　　　　　　　
+ * 　　　┃　 　　┗━━━┓ + +
+ * 　　　┃ 　　　　　　　┣┓
+ * 　　　┃ 　　　　　　　┏┛
+ * 　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　┃┫┫　┃┫┫
+ * 　　　　┗┻┛　┗┻┛+ + + +
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+# define POW2(X) (1 << (X))
+# define CKBIT(S,X) (((S) & POW2(X)) != 0)
+const double pi = acos(-1.0);
+const double eps = 1e-11;
+const int MOD = 1e9 + 7;
+template<class T> inline void ckmin(T &a,T b){ a = min(a, b); }
+template<class T> inline void ckmax(T &a,T b){ a = max(a, b); }
+template<class T> inline T sqr(T x){ return x * x; }
+# define SZ(A) ((int)A.size())
+# define LENGTH(A) ((int)A.length())
+# define MP make_pair
+# define PB push_back
+# define PPB pop_back
+# define PF push_front
+# define PPF pop_front
+# define LB lower_bound
+# define UB upper_bound
+# define RREP(i, a) for (int i = (a - 1); i >= 0; --i)
+# define FOR(i, a, b) for(int i = (a); i < (b); ++i) 
+# define REP(i, a) for(int i = 0; i < (a); ++i)
+# define ALL(A) A.begin(), A.end()
+# define RALL(A) A.rbegin(), A.rend()
+# define complete_unique(a) a.erase(unique(begin(a),end(a)),end(a))
+# define F first
+# define S second
+# define L left
+# define R right
+# define V vector 
+template<class T> int CMP(T a[], const T b[], int n) { return memcmp(a, b, n * sizeof(T)); }
+template<class T> void COPY(T a[], const T b[], int n) { memcpy(a, b, n * sizeof(T)); }
+template<class T> void SET(T a[], int val, int n) { memset(a, val, n*sizeof(T)); }
+using uint = unsigned int;
+using LL = long long;
+using ULL = unsigned long long;
+using PII = pair<int, int>;
+using VPII = vector<PII>;
+using PLL = pair<LL, LL>;
+using VPLL = vector<PLL>;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VD = vector<double>;
+using VDD = vector<VD>;
+using VS = vector<string>;
+using VVS = vector<VS>;
+
+
+
+
+
+// int main() {
+//     Solution sol;
+//     // VI nums = {};
+//     // VVI nums = {};
+//     auto ans = sol.();
+//     // cout << ans << endl;
+//     // REP(i, SZ(ans)) cout << ans[i] << endl;
+//     // REP(i, SZ(ans)) REP(j, SZ(ans[0])) cout << ans[i][j] << endl;
+//     system("pause");
+//     return 0;
+// }
