46901b3e65e132d93466d3a2394ff184ed2bb849
sunzhaoc <sunzhaoc_2011@163.com>
2021-07-12 08:57:53 +0800

    master

--- Leetcode/275. H-Index II.cpp
+++ Leetcode/275. H-Index II.cpp
@@ -3,7 +3,7 @@
  * @Version: 1.0
  * @Autor: 冰凝水
  * @Date: 2021-03-30 16:24:03
- * @LastEditTime: 2021-07-12 08:37:10
+ * @LastEditTime: 2021-07-12 08:56:04
  * @FilePath: \Leetcode\Leetcode\275. H-Index II.cpp
  */
 /*
@@ -91,10 +91,10 @@ using VVS = vector<VS>;
 
 /*
 RESULT: Accept
-TIME:   ms    BEAT: %    O(n) = 
-MEMORY: MB    BEAT: %    O(n) = 
-LAST EDIT TIME: 
-Description: 
+TIME:     16ms    BEAT: 89.59%    O(n) = 
+MEMORY: 18.2MB    BEAT: 16.68%    O(n) = 
+LAST EDIT TIME: 2021年7月12日8:55:10
+Description: 麻烦。
 */
 
 class Solution {
@@ -104,10 +104,12 @@ public:
         int l = 0, r = n - 1;
         while (l < r) {
             int mid = l + r >> 1;
-            if (citations[n - mid] >= mid) l = mid;
-            else r = mid - 1;
+            // 次数、数量
+            if (citations[mid] >= n - mid) r = mid;
+            else l = mid + 1;
         }
-        return r;
+        if (citations[l] >= n - l) return n - l;
+        return 0;
     }
 };
 
@@ -120,18 +122,18 @@ LAST EDIT TIME: 2021年3月30日18:57:22
 Description: Y总。
 */
 
-class Solution {
-public:
-    int hIndex(vector<int>& nums) {
-        int l = 0, r = nums.size();
-        while (l < r) {
-            int mid = l + r + 1 >> 1;
-            if (nums[nums.size() - mid] >= mid) l = mid;
-            else r = mid - 1;
-        }
-        return r;
-    }
-}; 
+// class Solution {
+// public:
+//     int hIndex(vector<int>& nums) {
+//         int l = 0, r = nums.size();
+//         while (l < r) {
+//             int mid = l + r + 1 >> 1;
+//             if (nums[nums.size() - mid] >= mid) l = mid;
+//             else r = mid - 1;
+//         }
+//         return r;
+//     }
+// }; 
 
 
 /*
@@ -142,22 +144,23 @@ LAST EDIT TIME: 2021年3月30日16:58:43
 Description: 二分。二刷。前一次是python。很难。。。不会。
 */
 
-class Solution {
-public:
-    int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        if (n == 0) return 0;
-        if (citations[n - 1] == 0) return 0;
-        int l = 0, r = n - 1;
-        while (l < r) {
-            int mid = (r - l) / 2 + l;
-            if (n - mid == citations[mid]) return n - mid; // n - mid 论文数
-            if (n - mid < citations[mid]) r = mid;
-            else l = mid + 1;
-        }
-        return n - l;
-    }
-};
+// class Solution {
+// public:
+//     int hIndex(vector<int>& citations) {
+//         int n = citations.size();
+//         if (n == 0) return 0;
+//         if (citations[n - 1] == 0) return 0;
+//         int l = 0, r = n - 1;
+//         while (l < r) {
+//             int mid = (r - l) / 2 + l;
+//             if (n - mid == citations[mid]) return n - mid; // n - mid 论文数
+//             if (n - mid < citations[mid]) r = mid;
+//             else l = mid + 1;
+//         }
+//         return n - l;
+//     }
+// };
+
 
 // @lc code=end
 
